6ecadb80e00055911717b912b3e2352c6fb11fd9
amisto <amisto@yandex.ru>
2023-03-22 23:55:59 +0300

    Lab 5 Static libraries

---
+++ lab_5/01_another_directory_static_library/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "sum.h"
+#include "mul.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << sum(40, 2) << " " << mul(6, 7) << endl;
+    return 0;
+}
